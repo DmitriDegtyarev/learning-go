c9400bea4e13d8474524b65b17b0dc91af73bcbc
Dmitry Degtyarev <68484737+DmitriDegtyarev@users.noreply.github.com>
2021-08-25 13:17:59 +0300

    Create 49-practice.correlate-function-value-to-type.go

---
+++ head-first/49-practice.correlate-function-value-to-type.go
@@ -0,0 +1,7 @@
+// reflect.TypeOf(25) - int
+// reflect.TypeOf(true) - bool
+// reflect.TypeOf(5.2) - float64
+// reflect.TypeOf(1) - int
+// reflect.TypeOf(false) - bool
+// reflect.TypeOf(1.0) - float64
+// reflect.TypeOf("hello") - string
