fe1b56bedcbc16444670676ae217e6bde1676b3a
Dmitry Degtyarev <68484737+DmitriDegtyarev@users.noreply.github.com>
2021-08-26 13:22:26 +0300

    Update 49-practice.correlate-function-value-to-type.go

--- head-first/49-practice.correlate-function-value-to-type.go
+++ head-first/49-practice.correlate-function-value-to-type.go
@@ -1,7 +1,28 @@
-// reflect.TypeOf(25) - int
-// reflect.TypeOf(true) - bool
-// reflect.TypeOf(5.2) - float64
-// reflect.TypeOf(1) - int
-// reflect.TypeOf(false) - bool
-// reflect.TypeOf(1.0) - float64
-// reflect.TypeOf("hello") - string
+// 13-19: объявляем переменные, указываем их тип и значение.
+//
+// 21-27: выводим на экран типы переменных.
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var a int = 25
+	var b bool = true
+	var c float64 = 5.2
+	var d int = 1
+	var e bool = false
+	var f float64 = 1.0
+	var g string = "hello"
+
+	fmt.Println(reflect.TypeOf(a))
+	fmt.Println(reflect.TypeOf(b))
+	fmt.Println(reflect.TypeOf(c))
+	fmt.Println(reflect.TypeOf(d))
+	fmt.Println(reflect.TypeOf(e))
+	fmt.Println(reflect.TypeOf(f))
+	fmt.Println(reflect.TypeOf(g))
+}
